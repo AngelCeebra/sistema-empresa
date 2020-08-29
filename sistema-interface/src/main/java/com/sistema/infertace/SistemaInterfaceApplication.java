e64ea425a1bb924e073b76a2e57f615cd4a57aa6
Angel <angel@MBP-de-Angel.huawei.net>
2020-08-29 18:49:18 -0500

    sistemas interface y legado running

---
+++ sistema-interface/src/main/java/com/sistema/infertace/SistemaInterfaceApplication.java
@@ -0,0 +1,13 @@
+package com.sistema.infertace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaInterfaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaInterfaceApplication.class, args);
+	}
+
+}
